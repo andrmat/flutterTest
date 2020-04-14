e8eb289180ece87cec496f0b93559c9e771ac62d
andrmat <mateuszagda@gmail.com>
2020-04-14 22:44:45 +0200

    .

---
+++ lib/screens/tester.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+
+class tester extends StatefulWidget {
+  @override
+  _testerState createState() => _testerState();
+}
+
+class _testerState extends State<tester> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('TestBar'),),
+    );
+  }
+}
